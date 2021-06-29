22a0efd14985f8dc7b7ec359168e837bd91dcfbd
Max0872 <maxmay0872@gmail.com>
2021-06-29 19:19:20 +0300

    add contact class

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+       WebDriver wd;
+        @BeforeMethod
+        public void precondition() {
+        wd =new ChromeDriver();
+            // login before
+        }
+        @Test
+        public void startGoogle(){
+            wd.get("https://www.google.co.il/");
+            //add new contact
+        }
+        @Test
+        public void loginTest(){
+            //add new contact
+        }
+        @AfterTest
+        public void postCondition(){
+        wd.close();
+        }
+
+
+}
